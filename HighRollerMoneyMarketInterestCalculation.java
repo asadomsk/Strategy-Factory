37edf2ffdf719b6e2ae488575737c9b497da389a
Alexandra Sadomsky <asadomsk@users.noreply.github.com>
2020-07-06 15:15:50 +0300

    Add files via upload

---
+++ HighRollerMoneyMarketInterestCalculation.java
@@ -0,0 +1,7 @@
+class HighRollerMoneyMarketInterestCalculation implements InterestCalculationStrategy {
+
+    @Override
+    public double calculateInterest(double accountBalance) {
+        return accountBalance < 100000.00 ? 0 : accountBalance * (0.075/12);
+    }
+}
---
+++ MoneyMarketInterestCalculation.java
@@ -0,0 +1,8 @@
+
+class MoneyMarketInterestCalculation implements InterestCalculationStrategy {
+
+    @Override
+    public double calculateInterest(double accountBalance) {
+        return accountBalance * (0.06/12);
+    }
+}
