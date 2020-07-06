628c41872a7789a9ac2635454c9368d5883bb4d3
Alexandra Sadomsky <asadomsk@users.noreply.github.com>
2020-07-06 14:19:15 +0300

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,10 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		InterestCalculator ic = new InterestCalculator();
+		System.out.println(ic.calculateInterest(AccountTypes.CURRENT, 5000));
+	}
+
+}
